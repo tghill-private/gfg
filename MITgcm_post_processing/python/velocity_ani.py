0c542d030deff48eb623d32bf1ae620c5b1e168e
Timothy Gordon Hill <tghill@bow.math.private.uwaterloo.ca>
2018-05-09 09:52:15 -0400

    Work on fixing animation

--- MITgcm_post_processing/python/velocity_ani.py
+++ MITgcm_post_processing/python/velocity_ani.py
@@ -1,9 +1,8 @@
 """
 
-    module ice_plt_ani.py
+    module velocity_ani.py
 
-    Plot sea ice fraction with overlaid wind direction field from MITgcm
-    model runs.
+    Animate just the ice velocity field
 
     This module makes animations from converted netCDF4 files, which should have
     been converted from the binary .data files from datnetcdf.py
@@ -47,11 +46,11 @@ def animate(iters, gifname, namespec = 'output_{iter}.nc', vmin = 0.2, vmax = 1,
 
     # get the grids from the first nc file
     ncdata = _getdataset(iters[0], namespec)
-    X, Y = np.meshgrid(np.array(ncdata['x']), np.array(ncdata['y']))
-
-    icefract = np.array(ncdata['ice_fract']).reshape(X.shape)
-    uice = np.array(ncdata['UICE']).reshape(X.shape)
-    vice = np.array(ncdata['VICE']).reshape(X.shape)
+    # X, Y = np.meshgrid(np.array(ncdata['x']), np.array(ncdata['y']))
+    X, Y = np.mgrid[:2*np.pi:10j,:2*np.pi:5j]    # uice = np.array(ncdata['UICE']).reshape(X.shape)
+    # vice = np.array(ncdata['VICE']).reshape(X.shape)
+    uice = np.sin(X)
+    vice = np.cos(X)
     filename = ncdata.filepath()
     ncdata.close()
 
@@ -59,9 +58,10 @@ def animate(iters, gifname, namespec = 'output_{iter}.nc', vmin = 0.2, vmax = 1,
     gs = gridspec.GridSpec(1, 1)
     ax = plt.subplot(gs[0, 0])
 
-    pcolor = ax.pcolormesh(X, Y, icefract, cmap = cmap, vmin = vmin, vmax = vmax)
-    quiverplot = ax.quiver(X[::10, ::10], Y[::10, ::10],
-                            uice[::10, ::10], vice[::10, ::10], scale = 0.001, pivot = "middle")
+    step = 1
+    quiverplot = ax.quiver(X[::step, ::step], Y[::step, ::step],
+                            uice[::step, ::step], vice[::step, ::step],
+                            pivot ='mid')
     ###########
 
     ax.set_xlabel('X [m]')
@@ -76,31 +76,33 @@ def animate(iters, gifname, namespec = 'output_{iter}.nc', vmin = 0.2, vmax = 1,
     #     return pcolor, quiverplot
 
     def animate(iter):
-        iterdata = _getdataset(iter, namespec)
-        iter_icefract = np.array(iterdata['ice_fract']).reshape(X.shape)
-        iter_uice = np.array(iterdata['UICE']).reshape(X.shape)
-        iter_vice = np.array(iterdata['VICE']).reshape(X.shape)
-        iterdata.close()
-
-        # see https://stackoverflow.com/questions/18797175/animation-with-pcolormesh-routine-in-matplotlib-how-do-i-initialize-the-data
-        iter_icefract = iter_icefract[:-1, :-1]
-
-        pcolor.set_array(iter_icefract.ravel())
-        U = iter_uice
-        V = iter_vice
-        U = U[::10, ::10]
-        V = V[::10, ::10]
+        # iterdata = _getdataset(iter, namespec)
+        # iter_icefract = np.array(iterdata['ice_fract']).reshape(X.shape)
+        # iter_uice = np.array(iterdata['UICE']).reshape(X.shape)
+        # iter_vice = np.array(iterdata['VICE']).reshape(X.shape)
+        #
+        # # see https://stackoverflow.com/questions/18797175/animation-with-pcolormesh-routine-in-matplotlib-how-do-i-initialize-the-data
+        # iter_icefract = iter_icefract[:-1, :-1]
+        #
+        U = np.sin(X + 0.1*iter)
+        V = np.cos(Y + 0.1*iter)
+        U = U[::step, ::step]
+        V = V[::step, ::step]
+        # print(U.shape)
+        # print(V.shape)
+        # print(U)
         quiverplot.set_UVC(U, V)
         # quiverplot.set_UVC( iter_uice[::2, ::2].ravel(), iter_vice[::2, ::2].ravel())
-        return (pcolor, quiverplot)
+        # iterdata.close()
+        return quiverplot,
 
     gs.tight_layout(fig)
 
-    anim = animation.FuncAnimation(fig,animate,frames=iters,interval=10000,blit=False,repeat=False)
+    anim = animation.FuncAnimation(fig,animate,frames=20,interval=10000,blit=False,repeat=False)
 
     # Then save the gif using ImageMagick writer
     writer = animation.ImageMagickFileWriter(fps = 2.5)
     anim.save(gifname, writer=writer)
 
 if __name__ == "__main__":
-    animate([0, 12, 24, 36, 48], 'myani.gif')
+    animate([0, 12, 24, 36, 48], 'myveloani.gif')
